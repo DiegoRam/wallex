e55d27ae40dbd53fb4669859fea670a14ee1b8ea
diegoram <diego@diegoram.me>
2017-06-06 11:37:14 -0300

    added trading template

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,8 +7,13 @@ class Application extends Controller {
   def index = Action {
     Ok(views.html.index("wallex->Dashboard"))
   }
+
   def funds = Action {
     Ok(views.html.funds("wallex->MyFunds"))
+  }
+
+  def trade = Action {
+    Ok(views.html.trade("wallex->Trading"))
   }
 
 }
