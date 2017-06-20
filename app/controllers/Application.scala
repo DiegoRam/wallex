1ce4a068129a7b7e37ba4167f479edea315721e2
diegoram <diego@diegoram.me>
2017-06-20 17:11:02 -0300

    added more mocks

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,19 +5,19 @@ import play.api.mvc._
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("wallex->Dashboard"))
+    Ok(views.html.index("wallix->Dashboard"))
   }
 
   def funds = Action {
-    Ok(views.html.funds("wallex->MyFunds"))
+    Ok(views.html.funds("wallix->MyFunds"))
   }
 
   def trade = Action {
-    Ok(views.html.trade("wallex->Trading"))
+    Ok(views.html.trade("wallix->Trading"))
   }
 
   def ifttt = Action {
-    Ok(views.html.ifttt("wallex->Coding"))
+    Ok(views.html.ifttt("wallix->Coding"))
   }
 
   def login = Action {
@@ -27,4 +27,19 @@ class Application extends Controller {
   def schedule = Action {
     Ok(views.html.schedule("Schedule"))
   }
+
+  def send = Action {
+    Ok(views.html.send("wallix -> Send"))
+  }
+
+  def send2address = Action {
+    Ok(views.html.send2address("wallix->send2address"))
+  }
+
+  def send2Service = Action {
+    Ok(views.html.send2Service("wallix->send2Service"))
+  }
+
+  def recommend = TODO
+  def settings = TODO
 }
